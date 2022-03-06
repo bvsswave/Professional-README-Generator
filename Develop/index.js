18f53e0476dc4b6a04f56e01bc4fa7480a77fe90
bvsswave <tannershahan@gmail.com>
2022-03-05 17:45:04 -0700

    Updated index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,33 @@
 // TODO: Include packages needed for this application
+// variables
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { generateMakrdown } = require('./utils/generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = (fileName, data) => {
+
+    fs.writeToFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+            console.log('File has been created')
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    promptProject ()
+    .then(input =>{
+        return generateMarkdown(input);
+    })
+}
 
 // Function call to initialize app
 init();
