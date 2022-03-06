aaf2fe4f44b36890475512e633fd7427ea6a2d74
bvsswave <tannershahan@gmail.com>
2022-03-05 17:14:42 -0700

    updated Markdown.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -8,7 +8,7 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+funct [ion renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 
@@ -27,6 +27,31 @@ function generateMarkdown(data) {
   * [License](#license)
   * [Tests](#tests)
   * [Questions](#questions)
+
+  ## [Description]
+
+  ${data.description}
+
+  ## [usage]
+
+  ${data.usage}
+
+  ## [License]
+
+  ${data.license}
+
+  ## [Contributing]
+
+  ${data.contribute}
+
+  ## [Tests]
+  ${data.tests}
+
+  ## [Questions]
+
+  If you have questions please contact me bellow:
+
+  [Email: ${data.email}]
 
 
 `;
